ee2ccbc3b4ee48423efe1f40b0fc1c89e5028f90
VictorPeters03 <91067568+VictorPeters03@users.noreply.github.com>
2022-02-16 16:50:18 +0100

    Robot rijdt nu rechtdoor en kan zwart herkennen

--- arduinorobot/arduinorobot.ino
+++ arduinorobot/arduinorobot.ino
@@ -5,18 +5,36 @@ int linksAchter = 18;
 int linksVoor = 5;
 int rechtsAchter = 16;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(rechtsVoor, OUTPUT);
-  pinMode(rechtsAchter, OUTPUT);
-  pinMode(linksVoor, OUTPUT);
-  pinMode(linksAchter, OUTPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(rechtsVoor, 200);
-  analogWrite(linksVoor, 200);
+void driveForwardStraight(int rechtsVoor, int linksVoor, int rechtsAchter, int linksAchter) 
+{
+  analogWrite(rechtsVoor, 212);
+  analogWrite(linksVoor, 187);
   analogWrite(rechtsAchter, LOW);
   analogWrite(linksAchter, LOW); 
+}
+
+//void setup() {
+//  // put your setup code here, to run once:
+//  pinMode(rechtsVoor, OUTPUT);
+//  pinMode(rechtsAchter, OUTPUT);
+//  pinMode(linksVoor, OUTPUT);
+//  pinMode(linksAchter, OUTPUT);
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+////  driveForwardStraight();
+//
+//}
+
+int Sensor = 39; 
+int sensorValue = 0; 
+
+void setup () {
+  pinMode(Sensor, INPUT);
+  Serial.begin (9600);
+}
+void loop () {
+  sensorValue = analogRead (Sensor);
+  Serial.println (sensorValue, DEC);
 }
