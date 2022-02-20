7401dfbba5bab999d6b4481dc702bc0236956b80
VictorP03 <crapowner321@gmail.com>
2022-02-20 14:21:52 +0100

    commit voorafgaand aan veranderingen

--- arduinorobot/arduinorobot.ino
+++ arduinorobot/arduinorobot.ino
@@ -22,8 +22,8 @@ void driveForwardStraight(int rechtsVoor, int linksVoor, int rechtsAchter, int l
 
 void showTapeOutput(int sensor, int sensor2)
 {
-  sensorValue = analogRead (Sensor);
-  sensorValue2 = analogRead (Sensor2);
+  sensorValue = digitalRead (sensor);
+  sensorValue2 = digitalRead (sensor2);
   Serial.print ("Right sensor: ");
   Serial.print (sensorValue, DEC);
   Serial.println("");
@@ -32,8 +32,20 @@ void showTapeOutput(int sensor, int sensor2)
   Serial.println("");
 }
 
-void driveOverTape() 
+void driveOverTape(int sensor, int sensor2) 
 {
+  sensorValue = digitalRead (sensor);
+  sensorValue2 = digitalRead (sensor2);
+  if (sensorValue == LOW && sensorValue2 == LOW)
+  {
+    rechtsVoor += 20;
+    linksVoor -= 20;
+  }
+  else if (sensorValue2 < 2000 && sensorValue > 2000)
+  {
+    rechtsVoor -= 20;
+    linksVoor += 20;
+  }
   
 }
 
@@ -43,11 +55,14 @@ void setup() {
   pinMode(rechtsAchter, OUTPUT);
   pinMode(linksVoor, OUTPUT);
   pinMode(linksAchter, OUTPUT);
-  pinMode(Sensor, INPUT);
+  pinMode(rightSensor, INPUT);
+  pinMode(leftSensor, INPUT);
   Serial.begin (9600);
 }
 
 void loop() {
 //  driveForwardStraight(rechtsVoor, linksVoor, rechtsAchter, linksAchter);
   showTapeOutput(rightSensor, leftSensor);
+//  driveOverTape(rightSensor, leftSensor);
+  delay(1000);
 }
