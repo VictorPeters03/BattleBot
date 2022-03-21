2de217d8d78ea77f8a1700613f5dc91c54983d77
rickv21 <rick.vinke@student.nhlstenden.com>
2022-03-21 15:57:41 +0100

    Code opruiming. - De code is beter ingedeelt in functies. - Er zijn wat meer comments toegevoegd. - Ongebruikte libraries zijn weggehaald zodat de nodige ruimte kleiner is en het compilen sneller gaat.

--- race/race.ino
+++ race/race.ino
@@ -1,9 +1,8 @@
 #include <Arduino.h>
 #include <analogWrite.h>
-#include <MPU9250.h>
-#include <Adafruit_VL53L0X.h>
+//#include <MPU9250.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
+//#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -22,33 +21,13 @@ int leftSensor = 34; //Dit is de rechtersensor bij driveOverTape 2 en 3
 int rightSensorValue = 0;
 int leftSensorValue = 0; 
 
-//adafruit vl53l0x
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-//VL53L0X_RangingMeasurementData_t measureDistance;
-
-//Adafruit SSD1306
+//Adafruit SSD1306 (display)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-/*void driveForwardStraight(int rechtsVoor, int linksVoor, int rechtsAchter, int linksAchter) 
+void showOutput(int directionMode)
 {
-  analogWrite(rechtsVoor, LOW);
-  analogWrite(linksVoor, LOW);
-  analogWrite(rechtsAchter, 187);
-  analogWrite(linksAchter, 196); 
-}*/
-
-void drive(int linksVoorSnelheid, int rechtsVoorSnelheid, int linksAchterSnelheid, int rechtsAchterSnelheid)
-{
-  analogWrite(linksVoor, linksVoorSnelheid);
-  analogWrite(rechtsVoor, rechtsVoorSnelheid);
-  analogWrite(linksAchter, linksAchterSnelheid);
-  analogWrite(rechtsAchter, rechtsAchterSnelheid);
-}
-
-void showOutput(int sensor, int sensor2, int directionMode)
-{
-  rightSensorValue = analogRead (sensor);
-  leftSensorValue = analogRead (sensor2);
+  //rightSensorValue = analogRead (rightSensorValue);
+  //leftSensorValue = analogRead (leftSensorValue);
   display.clearDisplay();
   
   display.setTextSize(1);
@@ -77,77 +56,93 @@ void showOutput(int sensor, int sensor2, int directionMode)
   //display.clearDisplay();
 }
 
+void moveForward(){
+    showOutput(1);
+    analogWrite(linksVoor, 190);
+    analogWrite(rechtsVoor, 190);
+    analogWrite(linksAchter, LOW);
+    analogWrite(rechtsAchter, LOW);
+}
+
+void moveBackwards(){
+    showOutput(4);
+     //drive(160, 163, LOW, LOW);
+    analogWrite(linksVoor, LOW);
+    analogWrite(rechtsVoor, LOW);
+    analogWrite(linksAchter, 190);
+    analogWrite(rechtsAchter, 190);
+}
+
+void moveLeft(){
+    showOutput(3);
+    //drive(LOW, 193, 196, LOW);
+    analogWrite(linksVoor, LOW);
+    analogWrite(rechtsVoor, 190);
+    analogWrite(linksAchter, LOW);
+    analogWrite(rechtsAchter, LOW);
+}
+
+void moveRight(){
+    showOutput(2);
+    //drive(190, LOW, LOW, 197);
+    analogWrite(linksVoor, 190);
+    analogWrite(rechtsVoor, LOW);
+    analogWrite(linksAchter, LOW);
+    analogWrite(rechtsAchter, LOW);
+}
+
 void race(int sensor, int sensor2, int rechtsVoor, int linksAchter, int linksVoor, int rechtsAchter) 
 {
-  /*if (measureDistance.RangeStatus != 4) {
-    //Noodstop
-    if(measureDistance.RangeMilliMeter < 100){
-      return;
-    }
-  }*/
   rightSensorValue = analogRead (sensor);
   leftSensorValue = analogRead (sensor2);
   //Vooruit??
   if (rightSensorValue > 120 && leftSensorValue > 120)
   {
-    showOutput(sensor, sensor2, 1);
-    //drive(LOW, LOW, 176, 167);
-    drive(190, 190, LOW, LOW);
+    moveForward();
   }
   //Rechts?
   else if (rightSensorValue < 120 && leftSensorValue > 120)
   {
-    showOutput(sensor, sensor2, 2);
-    
-    //drive(LOW, 193, 196, LOW);
-    drive(LOW, 190, LOW, LOW);
+    moveRight();
   }
   //Links
   else if (rightSensorValue > 120 && leftSensorValue > 120)
   {
-    showOutput(sensor, sensor2, 3);
-    //drive(190, LOW, LOW, 197);
-    drive(190, LOW, LOW, LOW);
+    moveLeft();
   }
   //Achteruit??
   else if (rightSensorValue < 120 && leftSensorValue < 120) 
   {
-    showOutput(sensor, sensor2, 4);
-     //drive(160, 163, LOW, LOW);
-    drive(LOW, LOW, 190, 190);
+    moveBackwards();
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  //Setup the motors
   pinMode(rechtsVoor, OUTPUT);
   pinMode(rechtsAchter, OUTPUT);
   pinMode(linksVoor, OUTPUT);
   pinMode(linksAchter, OUTPUT);
+  //Set all motors to 0 so they stop.
   analogWrite(linksVoor, 0);
   analogWrite(rechtsVoor, 0);
   analogWrite(linksAchter, 0);
   analogWrite(rechtsAchter, 0);
+  //Setup the IR sensors.
   pinMode(rightSensor, INPUT);
   pinMode(leftSensor, INPUT);
-  
-  Serial.begin (115200);
+
+  //Serial.begin (115200);
 
   //Start serial monitor
-  while (!Serial) 
+  /*while (!Serial) 
   {
     delay(1);
-  }
+  }*/
 
   display.begin(SSD1306_SWITCHCAPVCC, 0X3C);
   display.display();
-
-  //check if VL53L0X starts up
-  if (!lox.begin())
-  {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-  }
+  
   delay(2000);
   display.clearDisplay();
   display.display();
@@ -156,13 +151,8 @@ void setup() {
 void loop() {
   if (!isStarted)
   {
-    //driveForwardStraight(rechtsVoor, linksVoor, rechtsAchter, linksAchter);
     delay(1000);
     isStarted = 1;
   }
-//  showTapeOutput(rightSensor, leftSensor);
-//  driveOverTape(rightSensor, leftSensor, rechtsVoor, linksAchter, linksVoor, rechtsAchter);
   race(rightSensor, leftSensor, rechtsVoor, linksAchter, linksVoor, rechtsAchter);
-//  delay(1000);
-//  driveForwardStraight(rechtsVoor, linksVoor, rechtsAchter, linksAchter);
 }
