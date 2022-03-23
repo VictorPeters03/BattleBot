a446faa9cf8754404e7d90d0132991726b779dab
rickv21 <rick.vinke@student.nhlstenden.com>
2022-03-23 16:38:48 +0100

    - De robot kan nu de test baan afleggen. - Er is een pauze systeem toegevoegd voor makkelijk testen van sensor waardes.

--- race/race.ino
+++ race/race.ino
@@ -21,10 +21,12 @@ int leftSensor = 34; //Dit is de rechtersensor bij driveOverTape 2 en 3
 int rightSensorValue = 0;
 int leftSensorValue = 0; 
 
+boolean paused = false;
+
 //Adafruit SSD1306 (display)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void showOutput(int directionMode)
+void printData(int directionMode)
 {
   //rightSensorValue = analogRead (rightSensorValue);
   //leftSensorValue = analogRead (leftSensorValue);
@@ -47,45 +49,54 @@ void showOutput(int directionMode)
       display.print ("links (3)");
   } else if(directionMode == 4){
       display.print ("achteruit (4)");
+  } else if(directionMode == 5){
+      display.print ("stopped (5)");
   }
   display.println("");
   display.print("INF1C Robot");
   display.display();
-  delay(100);
-
+  
   //display.clearDisplay();
 }
 
 void moveForward(){
-    showOutput(1);
-    analogWrite(linksVoor, 190);
-    analogWrite(rechtsVoor, 190);
+    printData(1);
+    analogWrite(linksVoor, LOW);
+    analogWrite(rechtsVoor, LOW);
+    analogWrite(linksAchter, 222);
+    analogWrite(rechtsAchter, 225);
+}
+
+void moveBackwards(){
+    printData(4);
+     //drive(160, 163, LOW, LOW);
+    analogWrite(linksVoor, 222);
+    analogWrite(rechtsVoor, 225);
     analogWrite(linksAchter, LOW);
     analogWrite(rechtsAchter, LOW);
 }
 
-void moveBackwards(){
-    showOutput(4);
-     //drive(160, 163, LOW, LOW);
-    analogWrite(linksVoor, LOW);
-    analogWrite(rechtsVoor, LOW);
-    analogWrite(linksAchter, 190);
-    analogWrite(rechtsAchter, 190);
-}
-
 void moveLeft(){
-    showOutput(3);
+    printData(3);
     //drive(LOW, 193, 196, LOW);
     analogWrite(linksVoor, LOW);
-    analogWrite(rechtsVoor, 190);
-    analogWrite(linksAchter, LOW);
+    analogWrite(rechtsVoor, 225);
+    analogWrite(linksAchter, 225);
     analogWrite(rechtsAchter, LOW);
 }
 
 void moveRight(){
-    showOutput(2);
+    printData(2);
     //drive(190, LOW, LOW, 197);
-    analogWrite(linksVoor, 190);
+    analogWrite(linksVoor, 225);
+    analogWrite(rechtsVoor, LOW);
+    analogWrite(linksAchter, LOW);
+    analogWrite(rechtsAchter, 225);
+}
+
+void stopMove(){
+    printData(5);
+    analogWrite(linksVoor, LOW);
     analogWrite(rechtsVoor, LOW);
     analogWrite(linksAchter, LOW);
     analogWrite(rechtsAchter, LOW);
@@ -95,23 +106,52 @@ void race(int sensor, int sensor2, int rechtsVoor, int linksAchter, int linksVoo
 {
   rightSensorValue = analogRead (sensor);
   leftSensorValue = analogRead (sensor2);
+
+  if(rightSensorValue == 0 && leftSensorValue == 0){
+    if(!paused){
+       stopMove();
+       paused = true;
+       delay(1000);
+       return;
+    } else {
+      paused = false;
+      delay(1000);
+    }
+  }
+  else if(paused){
+    printData(5);
+    return;
+  }
+  
   //Vooruit??
-  if (rightSensorValue > 120 && leftSensorValue > 120)
+
+  //Wit is laag
+  //If both sensors don't see the line.
+  if (rightSensorValue < 1000 && leftSensorValue < 1000)
   {
     moveForward();
   }
   //Rechts?
-  else if (rightSensorValue < 120 && leftSensorValue > 120)
+  //If the right sensor sees the line.
+  else if (rightSensorValue > 1000 && leftSensorValue < 1000)
   {
+    stopMove();
+    delay(100);
     moveRight();
+    delay(100);
   }
   //Links
-  else if (rightSensorValue > 120 && leftSensorValue > 120)
+  //If the left sensor sees the line
+  else if (rightSensorValue < 1000 && leftSensorValue > 1000)
   {
+    stopMove();
+    delay(100);
     moveLeft();
+    delay(100);
   }
   //Achteruit??
-  else if (rightSensorValue < 120 && leftSensorValue < 120) 
+  //If both sensors see the line.
+  else if (rightSensorValue > 1000 && leftSensorValue > 1000)
   {
     moveBackwards();
   }
